afbdc113fed49c14ed0330af1f1b4084bf853736
mjvirusofficial <deepakravidas565@gmail.com>
2024-03-21 11:54:06 +0530

    setup completed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Mj
+          Learn React
         </a>
       </header>
     </div>
