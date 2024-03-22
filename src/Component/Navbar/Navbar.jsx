9dbef1bf8b4aed623230ed0cf12e8cd22199fc78
mjvirusofficial <deepakravidas565@gmail.com>
2024-03-22 10:54:02 +0530

    starting from mobile veiw

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
+import './Navbar.css'
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import logo from '../../Image/Logo.png'
 import girl from '../../Image/Character14.svg'
-import './Navbar.css'
-// import finger from '../../Image/drag.png'
-import { NavLink } from 'react-router-dom'
-
 
 function Navbar() {
 
@@ -15,31 +14,18 @@ function Navbar() {
         console.log('click')
     };
 
-
-    return (
-        <>
-            <div className="container mt-3 navbar" id='ForDesktop'>
-                <NavLink to='/'>
-                    <img className='logoimg' src={logo} alt="" />
-                </NavLink>
-                <div className="nav">
-                    <ul>
-                        <li><NavLink to='/' className='nav-link'><p>Home</p></NavLink></li>
-                        <li><NavLink to='/about' className='nav-link'><p>About Us</p></NavLink></li>
-                        <li><NavLink to='/exam' className='nav-link'><p>Exams <i class="fa-solid fa-angle-down"></i></p></NavLink></li>
-                        <li><NavLink to='/enquiry' className='nav-link'><p>Enquiry Form</p></NavLink></li>
-                        <li><NavLink to='/service' className='nav-link'><p>Services</p></NavLink></li>
-                        <li><NavLink to='/contact' className='nav-link'><p>Contact Us</p></NavLink></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="container-fluid mt-2" id='ForMobile' >
-                <div className="mobileNav">
-                    <NavLink to='/'>
+  return (
+    <>
+        <div className="container mobile">
+            <div className="mleft">
+                
+            <NavLink to='/'>
                         <img id='logo2' className='logoimg2' src={logo} alt="" />
                     </NavLink>
-                    <svg
+
+            </div>
+            <div className="mright">
+            <svg
                         onClick={handleClick}
                         className='barsvg'
                         width="46"
@@ -52,43 +38,10 @@ function Navbar() {
                         <rect y="11" width="24" height="2" fill="currentColor" />
                         <rect y="19" width="24" height="2" fill="currentColor" />
                     </svg>
-                </div>
-
-                {/* <div className="mobilemenu ">
-                    <div className='mobileUldiv' >
-                        <ul className={isClicked ? 'clickOnMobileUl' : 'mobileUl'}>
-                            <li><NavLink to='/' className='nav-link'><p>Home</p></NavLink></li>
-                            <li><NavLink to='/about' className='nav-link'><p>About Us</p></NavLink></li>
-                            <li><NavLink to='/exam' className='nav-link'><p>Exam</p></NavLink></li>
-                            <li><NavLink to='/enquiry' className='nav-link'><p>Enquiry Form</p></NavLink></li>
-                            <li><NavLink to='/service' className='nav-link'><p>Services</p></NavLink></li>
-                            <li><NavLink to='/contact' className='nav-link'><p>Contact Us</p></NavLink></li>
-                            <div className="girlimg">
-                                <img className='girl' src={girl} alt="" />
-                            </div>
-                        </ul>
-
-                    </div>
-
-                </div> */}
-
-
-                <div className={isClicked ? 'mobileMenu mt-3' : 'mobileMenu2 mt-3'}>
-                    <div className="mMenu">
-                        <NavLink to='/' className='nav-link'><p>Home</p></NavLink>
-                        <NavLink to='/about' className='nav-link'><p>About Us</p></NavLink>
-                        {/* <NavLink to='/exam' className='nav-link'><p>Exam</p></NavLink> */}
-                        <NavLink to='/enquiry' className='nav-link'><p>Enquiry Form</p></NavLink>
-                        <NavLink to='/service' className='nav-link'><p>Services</p></NavLink>
-                        <NavLink to='/contact' className='nav-link'><p>Contact Us</p></NavLink>
-                    </div>
-                    <div className="girlimg">
-                        <img className='girl' src={girl} alt="" />
-                    </div>
-                </div>
             </div>
-        </>
-    )
+        </div>
+    </>
+  )
 }
 
 export default Navbar
