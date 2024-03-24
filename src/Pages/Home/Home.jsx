262421539b5e6e5219e3a90198debbba590ff85c
mjvirusofficial <deepakravidas565@gmail.com>
2024-03-24 15:24:19 +0530

    if its not work then change in homepageDiv

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Home.css'
 import img1 from '../../Image/11669618_20943839.svg'
 import rocket from '../../Image/3d-business-space-rocket.png'
+import england from '../../Image/England.5d6762b582366cf35c6d.jpg'
 
 function Home() {
+
+
   return (
     <>
-      <div className="container">
-        <div className="row homepageDiv">
+      <div className="container homepageDiv">
+        <div className="row ">
           <div className="col-12 col-lg-6 col-md-12">
             <div className='headline m-0'>Welcome to Suvidha OverSeas <img className='rocket' src={rocket} alt="rocket" /></div>
             <div className="para">We Empower your Dreams</div>
@@ -25,6 +28,21 @@ function Home() {
             <img className='img1' src={img1} alt="" />
           </div>
         </div>
+      </div>
+
+      <div className="2ndDiv container">
+        <div className="nextDiv">
+          <div className="topStudy text-center">
+            Top Study Abroad Destination
+          </div>
+          <div className="topStudy2 text-center">
+            Study in best Universities accross the world
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+      
       </div>
     </>
   )
